b08d64c34ba9e7d695423190deba25ae3215e189
Rifat0001 <mahmudrifat764@gmail.com>
2023-07-28 04:04:49 +0600

    routes

--- src/components/Layers/DashBoard.jsx
+++ src/components/Layers/DashBoard.jsx
@@ -1,7 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Header from "../Shared/Header";
+import Footer from "../Shared/Footer";
+
 const DashBoard = () => {
     return (
         <div>
-
+            <Header></Header>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
