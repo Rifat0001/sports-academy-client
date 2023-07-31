db4d12b7b7d98abf01aaf24a0378d3728f19ca62
Rifat0001 <mahmudrifat764@gmail.com>
2023-08-01 00:29:31 +0600

    dashboard updated

--- src/components/Layers/DashBoard.jsx
+++ src/components/Layers/DashBoard.jsx
@@ -1,10 +1,12 @@
 import { useContext } from "react";
 import { NavLink, Outlet } from "react-router-dom";
 import { AuthContext } from "../Provider/AuthProvider";
-import { FaHome, FaHourglassHalf, FaHouseUser, FaReceipt, FaSchool, FaUserGraduate, FaUserShield, FaUserTie } from "react-icons/fa";
+import { FaChalkboard, FaChalkboardTeacher, FaHome, FaHourglassHalf, FaHouseUser, FaReceipt, FaSchool, FaUserGraduate, FaUserSecret, FaUserShield, FaUserTie, FaUsers, FaWind } from "react-icons/fa";
 
 const DashBoard = () => {
     const { user } = useContext(AuthContext);
+    const isAdmin = false;
+    const isInstructor = false;
     return (
         <div className="">
             <div className="drawer lg:drawer-open ">
@@ -21,37 +23,62 @@ const DashBoard = () => {
                 <div className="drawer-side">
                     <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
                     <ul className="menu p-4 w-80 h-full bg-primary text-white font-bold text-xl space-y-4">
+                        {isAdmin ? (
+                            <>
+                                <li>
+                                    <NavLink to="adminhome"> <FaUserSecret></FaUserSecret> Admin Home</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="manageclasses"> <FaSchool></FaSchool> Manage Classes</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="manageusers"> <FaUsers></FaUsers> Manage Users</NavLink>
+                                </li>
+                            </>
+                        ) : isInstructor ? (
+                            <>
+                                <li>
+                                    <NavLink to="instructorhome"> <FaChalkboardTeacher></FaChalkboardTeacher> Instructor Home</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="addclass"> <FaChalkboard></FaChalkboard> Add A Class</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="myclass"> <FaWind></FaWind> My Class</NavLink>
+                                </li>
 
-                        <>
-                            <div className="flex items-center justify-center py-8">
-                                <div className="w-20 text-center">
-                                    {user?.photoURL ? (
-                                        <img
-                                            src={`${user?.photoURL}`}
-                                            alt=""
-                                            className="rounded-full w-10"
-                                        />
-                                    ) : (
-                                        <FaUserShield size={30} color="#ff3811" />
-                                    )}
+                            </>
+                        ) : (
+                            <>
+                                <div className="flex items-center justify-center py-8">
+                                    <div className="w-20 text-center">
+                                        {user?.photoURL ? (
+                                            <img
+                                                src={`${user?.photoURL}`}
+                                                alt=""
+                                                className="rounded-full w-10"
+                                            />
+                                        ) : (
+                                            <FaUserShield size={30} color="#ff3811" />
+                                        )}
+                                    </div>
+                                    <h2 className=" ">{user?.displayName}</h2>
                                 </div>
-                                <h2 className="">{user?.displayName}</h2>
-                            </div>
-                            <hr />
-                            <li>
-                                <NavLink to="userhome"> <FaHouseUser></FaHouseUser> User Home</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="mycart"> <FaSchool></FaSchool> My Selected Class</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="enrolledclasses"> <FaUserGraduate></FaUserGraduate> My Enrolled Class</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to="paymenthistory"> <FaReceipt></FaReceipt> Payment History</NavLink>
-                            </li>
-                        </>
-
+                                <hr />
+                                <li>
+                                    <NavLink to="userhome"> <FaHouseUser></FaHouseUser> User Home</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="mycart"> <FaSchool></FaSchool> My Selected Class</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="enrolledclasses"> <FaUserGraduate></FaUserGraduate> My Enrolled Class</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="paymenthistory"> <FaReceipt></FaReceipt> Payment History</NavLink>
+                                </li>
+                            </>
+                        )}
                         <hr />
                         <li>
                             <NavLink to="/"> <FaHome></FaHome> Home Page</NavLink>
@@ -64,6 +91,7 @@ const DashBoard = () => {
                         </li>
                     </ul>
                 </div>
+
             </div>
         </div>
     );
